8db36bdfc832e3aa354b2f789ba218c80d108809
yurashalya <stallker9696@gmail.com>
2018-08-20 02:52:33 +0300

    Create link a navigation menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,35 @@
 <template>
 	<div>
-		<v-navigation-drawer app temporary></v-navigation-drawer>
+		<v-navigation-drawer app temporary v-model="sideNav">
+			 <v-list>
+          <v-list-tile
+			v-for="(link, i) in links"
+			:key="i"
+			:to="link.url"
+          >
+            <v-list-tile-action>
+              <v-icon> {{ link.icon }} </v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title v-text="link.title"></v-list-tile-title>
+            </v-list-tile-content>  
+          </v-list-tile>
+        </v-list>
+		</v-navigation-drawer>
   		<v-toolbar app  color="teal lighten-3"dark>
-  			<v-toolbar-side-icon></v-toolbar-side-icon>
+  			<v-toolbar-side-icon @click= "sideNav = !sideNav"></v-toolbar-side-icon>
 		    <v-toolbar-title>Online Store</v-toolbar-title>
 		    <v-spacer></v-spacer>
 		    <v-toolbar-items class="hidden-sm-and-down">
-		      <v-btn flat>Link One</v-btn>
+		      <v-btn flat
+				v-for="(link,i) in links"
+				:key="i"
+				:to="link.url"
+		      >
+				<v-icon left> {{ link.icon }} </v-icon>
+		      	{{ link.title }}
+		  		</v-btn>
 		      
 		    </v-toolbar-items>
   		</v-toolbar>
@@ -22,6 +45,17 @@
 
 <script>
 	export default {
-
+		data () {
+			return {
+				sideNav: false,
+				links: [
+					{ title: 'Login', icon: 'account_box', url: '/login' },
+					{ title: 'Register', icon: 'face', url: '/register' },
+					{ title: 'Cart', icon: 'shopping_cart', url: '/checkout' },
+					{ title: 'New Product', icon: 'add', url: '/new' },
+					{ title: 'My Products', icon: 'list', url: '/list' },
+				]
+			}
+		}
 	}
 </script>
