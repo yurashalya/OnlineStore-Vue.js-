0d94a55ce26a5342816f78a6a56b9097f983462a
yurashalya <stallker9696@gmail.com>
2018-08-20 05:11:05 +0300

    Implementing the login page with vuetify

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,11 +1,44 @@
 <template>
-	<v-container>
-		<v-layout row >
-			<v-flex xs12>
-				<h1>Login</h1>
-			</v-flex>
-		</v-layout>	
-	</v-container>
+	<v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md6>
+            <v-card class="elevation-12">
+              <v-toolbar color="teal lighten-3"dark>
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form ref="form" v-model="valid" lazy-validation>
+                  <v-text-field 
+                  prepend-icon="person" 
+                  name="e-mail" 
+                  label="E-mail" 
+                  type="email"
+                  v-model="email"
+                  :rules="emailRules"
+                  ></v-text-field>
+                  <v-text-field 
+                  prepend-icon="password" 
+                  name="password" 
+                  label="Password" 
+                  type="password"
+                  v-model="password"
+                  :counter="6"
+                  :rules="passwordRules"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                color="teal lighten-3"dark
+                @click='onSubmit'
+                :disabled='!valid'
+                >Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
 
 </template>
 
@@ -13,7 +46,27 @@
 	export default {
 		data () {
 			return {
-				
+				email: '',
+				password: '',
+				valid: false,
+				emailRules: [
+					v => !!v || 'E-mail is required',
+       				v => /.+@.+/.test(v) || 'E-mail must be valid'
+				],
+				passwordRules: [
+					v => !!v || 'Password is required',
+       				v => (v && v.length >=6) || 'Password must be equal or than 6 characters'
+				],
+			}
+		},
+		methods: {
+			onSubmit () {
+				if (this.$refs.form.validate()) {
+					const user = {
+						email: this.email,
+						password: this.password
+					}
+				}
 			}
 		}
 	}
